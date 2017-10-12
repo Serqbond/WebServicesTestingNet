1f2dca136ff4a435277b1a807f4f5d7693b79180
serqbond <serqbond@gmail.com>
2017-10-12 15:41:27 +0300

    Added comments to see on CI

--- RestAPITests/Tests/SpecflowTestsSteps/SeleniumSteps.cs
+++ RestAPITests/Tests/SpecflowTestsSteps/SeleniumSteps.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using RestAPITests.Tests.SpecflowTestsSteps.POM;
+using System;
 using TechTalk.SpecFlow;
 
 namespace RestAPITests.Tests.SpecflowTestsSteps
@@ -23,36 +24,42 @@ namespace RestAPITests.Tests.SpecflowTestsSteps
         public void GivenIOpenPage(string ulr)
         {            
             driver.Navigate().GoToUrl(ulr);
+            Console.WriteLine("GivenIOpenPage" + System.Threading.Thread.CurrentThread.Name);
         }
 
         [Given(@"I have entered '(.*)' into the bing search field")]
         public void GivenIHaveEnteredIntoTheBingSearchField(string searchText)
         {
             bingSearchPage.SerchTextField.SendKeys(searchText);
+            Console.WriteLine("GivenIHaveEnteredIntoTheBingSearchField" + System.Threading.Thread.CurrentThread.Name);
         }
 
         [Given(@"I have entered '(.*)' into the search field")]
         public void GivenIHaveEnteredIntoTheSearchField(string searchText)
         {            
             gogleSearchPage.SerchTextField.SendKeys(searchText);
+            Console.WriteLine("GivenIHaveEnteredIntoTheSearchField" + System.Threading.Thread.CurrentThread.Name);
         }
 
         [When(@"I press search button")]
         public void WhenIPressSearchButton()
         {
             gogleSearchPage.SerchTextField.SendKeys("\n");
+            Console.WriteLine("WhenIPressSearchButton" + System.Threading.Thread.CurrentThread.Name);
         }
 
         [When(@"I press search button on the bing page")]
         public void WhenIPressSearchButtonOnTheBingPage()
         {
             bingSearchPage.ButtonSearch.Click();
+            Console.WriteLine("WhenIPressSearchButtonOnTheBingPage" + System.Threading.Thread.CurrentThread.Name);
         }
 
         [Then(@"the result should contain '(.*)' on the bing page")]
         public void ThenTheResultShouldContainOnTheBingPage(string expectedtext)
         {
             Assert.DoesNotThrow(() => bingSearchPage.SearchResultLink(expectedtext));
+            Console.WriteLine("ThenTheResultShouldContainOnTheBingPage" + System.Threading.Thread.CurrentThread.Name);
         }
 
 
@@ -60,6 +67,7 @@ namespace RestAPITests.Tests.SpecflowTestsSteps
         public void ThenTheResultShouldContain(string expectedtext)
         {
             Assert.DoesNotThrow(() => gogleSearchPage.SearchResultLink(expectedtext));
+            Console.WriteLine("ThenTheResultShouldContain" + System.Threading.Thread.CurrentThread.Name);
         }
     }
 }
